3631b3c09ea1bac237faf92b283a6499ae24caa4
Matheus Alves Domingos <matheusad@hmmg.local>
2020-04-07 12:22:07 -0300

    Added disciplines update

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,14 +1,33 @@
 const User = require('../models/user')
 
-exports.userById = (req,res,next,id) => {
-    User.findById(id).exec((err,user) =>{
-        if(err || !user){
+exports.userById = async (req, res, next, id) => {
+  await User.findById(id).exec((err, user) => {
+        if (err || !user) {
             return res.status(400).json({
-                error:'User not found'
+                error: 'User not found'
             })
         }
         req.profile = user
         next()
 
+    })
+}
+
+exports.updateDiscipline = async (req, res) => {
+    await User.findById(req.profile._id).exec((err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: 'User not found'
+            })
+        }
+        console.log(req.disciplines)
+        console.log(req)
+        user.disciplines = req.body.disciplines
+        user.save((err, user) => {
+            if (err || !user) {
+                return res.status(400).json({ err: "Cannot update user" })
+            }
+            return res.status(200).json({ user })
+        })
     })
 }
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -6,13 +6,14 @@ const {
       isAuth,
  } = require("../controllers/auth");
 
-const {userById} = require("../controllers/user");
+const {userById,updateDiscipline} = require("../controllers/user");
 
 router.get('/secret/:userId',requireSignin,isAuth,   (req,res) => {
     res.json({
         user:req.profile
     })
 })
+router.post('/update/disciplines/:userId',/**add requiresignin and isauth */  updateDiscipline)
 
 router.param('userId', userById)
 
