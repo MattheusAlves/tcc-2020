52366318a9a64aacf2314babe5069670d2c608d5
Matheus Alves Domingos <matheusad@hmmg.local>
2020-06-30 14:05:04 -0300

    30/06

--- frontend/src/pages/Topics/Dashboard/style.js
+++ frontend/src/pages/Topics/Dashboard/style.js
@@ -1,22 +1,32 @@
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native'
+
+function TouchableCollors() {
+    const colors = ['#ff99e6', '#ff00ff', '#ff4d4d', '#1a75ff', '#66ffc2',
+        '#00e600', '#9999ff', '#33cccc']
+
+    return colors[Math.floor(Math.random() * 7)]
+}
 
 const styles = StyleSheet.create({
     item: {
         alignItems: "center",
-        backgroundColor: "#99ccff",
         flexGrow: 1,
         margin: 4,
         padding: 20,
         flexBasis: 0,
-        borderRadius:5
+        borderRadius: 5,
+       
     },
     text: {
-    fontSize:15,
-    fontFamily:'sans-serif-medium'
+        fontSize: 15,
+        fontFamily: 'sans-serif-medium'
     },
     itemEmpty: {
         backgroundColor: "transparent"
     },
 })
 
-export default styles
+export default TouchableCollors
+export { styles }
+// export default styles
