7ba3ba2ed10ca8efdbda0e8f97971716d7555117
Matheus Alves Domingos <matheusad@hmmg.local>
2020-07-06 13:58:51 -0300

    Added SVG

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,31 @@
-import React, { } from "react";
-import { Text } from 'react-native'
+import React, {useState } from "react";
 import { StatusBar } from "react-native";
 import 'react-native-gesture-handler';
 import { Provider as PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo'
 
 import { AuthProvider } from "./src/contexts/auth";
 import { LocationProvider } from './src/contexts/location'
-import commonStyles from "./src/commonStyles";
 
 import Routes from "./src/routes/index";
-
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'Markazi-Regular': require('./src/assets/fonts/Markazi Text Regular.ttf'),
+    'Roboto-Regular':require('./src/assets/fonts/Roboto_Slab_Regular.ttf')
+  });
+};
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false)
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
+    )
+  }
   return (
     <>
       <NavigationContainer>
--- frontend/src/pages/Topics/Dashboard/index.js
+++ frontend/src/pages/Topics/Dashboard/index.js
@@ -1,90 +1,116 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import { Card, Title, Button } from 'react-native-paper';
+import Svg, { Rect, Text as text, Path } from 'react-native-svg'
 
 import TouchableCollors, { styles } from './style';
 import api from "../../../services/api";
-import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView, RectButton } from 'react-native-gesture-handler';
 
+function WavyCard() {
+  const Path = ['M0,224L30,224C60,224,120,224,180,234.7C240,245,300,267,360,245.3C420,224,480,160,540,138.7C600,117,660,139,720,128C780,117,840,75,900,80C960,85,1020,139,1080,149.3C1140,160,1200,128,1260,96C1320,64,1380,32,1410,16L1440,0L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z', 'M0,96L48,96C96,96,192,96,288,117.3C384,139,480,181,576,170.7C672,160,768,96,864,90.7C960,85,1056,139,1152,149.3C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z', 'M0,64L34.3,64C68.6,64,137,64,206,80C274.3,96,343,128,411,122.7C480,117,549,75,617,53.3C685.7,32,754,32,823,42.7C891.4,53,960,75,1029,117.3C1097.1,160,1166,224,1234,218.7C1302.9,213,1371,139,1406,101.3L1440,64L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z', 'M0,160L34.3,133.3C68.6,107,137,53,206,69.3C274.3,85,343,171,411,170.7C480,171,549,85,617,90.7C685.7,96,754,192,823,202.7C891.4,213,960,139,1029,133.3C1097.1,128,1166,192,1234,213.3C1302.9,235,1371,213,1406,202.7L1440,192L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z']
+  const selectPath = Math.floor(Math.random() * 2)
+  function selectSvgPath(path,selectPath){
+    return Path[selectPath].toString()
+  }
+  const svg = <Svg
+    width='100'
+    height='100'
+    style={styles.svg}
+    viewBox="0 0 640 180">
+    <Path fill="#5000ca" fill-opacity="1" d={`${Path[selectPath].toString()}`} />
+  </Svg>
+  return (
+    svg
+   
+
+  )
+}
 function RenderComponent(props) {
-    const Component = []
-    console.log(props.data.length)
-    // console.log(props.data)
-    for (let i = 0; i < props.data.length; i++) {
-        Component.push(
-            <>
-                <Card style={[styles.item, {
+  const Component = []
+  console.log(props.data.length)
+  // console.log(props.data)
+  for (let i = 0; i < props.data.length; i++) {
+    Component.push(
+      <>
+        <Card style={[styles.item, {
 
-                    opacity: props.style.indexOf(i) > -1 ? .3 : 1
-                }]}
-                    onPress={() => props._onPress(i)}
-                    key={props.data[i]._id}>
-                    <Card.Content>
-                        <Title style={[styles.textCard, { opacity: 1 }]}>{props.data[i].disciplineName}</Title>
-                    </Card.Content>
-                </Card>
-            </>
-        )
+          opacity: props.style.indexOf(i) > -1 ? .3 : 1
+        }]}
+          onPress={() => props._onPress(i)}
+          key={props.data[i]._id}>
+          <Card.Content style={styles.cardContent}>
+            <WavyCard />
+            <Title style={[styles.textCard, { opacity: 1 }]}>{props.data[i].disciplineName}</Title>
 
-    }
-    return Component
+          </Card.Content>
+
+        </Card>
+      </>
+    )
+
+  }
+  return Component
 
 }
 
 const Dashboard = () => {
-    const [opacity, setOpacity] = useState([])
-    const [data, setData] = useState()
-    const [disciplines, setDisciplines] = useState([{}])
-    const [selectDiscipline, setSelectDiscipline] = useState([])
-    const Component = []
-    function _onPressCard(num) {
-        console.log("press button")
-        if (opacity.indexOf(num) > -1) {
-            setOpacity([...opacity.filter(item => item != num)])
-            // setSelectDiscipline([...selectDiscipline.splice(selectDiscipline.indexOf(id), 1)])
-        } else
-            setOpacity([...opacity, num])
-        // setSelectDiscipline([...selectDiscipline, id])
+  const [opacity, setOpacity] = useState([])
+  const [data, setData] = useState()
+  const [disciplines, setDisciplines] = useState([{}])
+  const [selectDiscipline, setSelectDiscipline] = useState([])
+  const Component = []
+  function _onPressCard(num) {
+    console.log("press button")
+    if (opacity.indexOf(num) > -1) {
+      setOpacity([...opacity.filter(item => item != num)])
+      // setSelectDiscipline([...selectDiscipline.splice(selectDiscipline.indexOf(id), 1)])
+    } else
+      setOpacity([...opacity, num])
+    // setSelectDiscipline([...selectDiscipline, id])
 
-    }
-    useEffect(() => {
-        api.get("/disciplines/list").then((disciplinesArray) => {
-            if (disciplinesArray) {
-                console.log("Entrou if disciplines array")
-                const arrayLength = disciplinesArray.data.disciplines.length
-                setDisciplines(disciplinesArray.data.disciplines)
-            }
-        }).catch(function (error) {
-            console.log(error)
-        })
-        setOpacity([])
-    }, [])
-    return (
-        // <SafeAreaView>
-        <>
-            {disciplines.length > 1 ? (
-                <View style={styles.appContainer}>
-                    <View style={styles.viewHeader}>
-                        <Text style={styles.textHeader}>Escolha suas áreas de interesse</Text>
-                    </View>
-                    <View style={styles.containerScrollView}>
-                        <ScrollView style={styles.scrollView}>
-                            <View style={styles.container}>
-                                <RenderComponent data={disciplines} style={opacity} _onPress={_onPressCard} />
-                            </View>
-                        </ScrollView>
-                    </View>
-                    <Button mode="contained" dark loading={false} onPress={() => { }}>Salvar</Button>
-                </View>
+  }
+  useEffect(() => {
+    api.get("/disciplines/list").then((disciplinesArray) => {
+      if (disciplinesArray) {
+        console.log("Entrou if disciplines array")
+        const arrayLength = disciplinesArray.data.disciplines.length
+        setDisciplines(disciplinesArray.data.disciplines)
+      }
+    }).catch(function (error) {
+      console.log(error)
+    })
+    setOpacity([])
+  }, [])
+  return (
+    // <SafeAreaView>
+    <>
+      {disciplines.length > 1 ? (
+        <View style={styles.appContainer}>
+          <View style={styles.viewHeader}>
+            <Text style={styles.textHeader}>Escolha suas áreas de interesse</Text>
+
+          </View>
+          <View style={styles.containerScrollView}>
+            <ScrollView style={styles.scrollView}>
+              <View style={styles.container}>
+                <RenderComponent data={disciplines} style={opacity} _onPress={_onPressCard} />
+                {/* <WavyCard style={styles.svg}/> */}
+              </View>
+            </ScrollView>
+          </View>
+
+          <Button mode="contained" dark loading={false} onPress={() => { }}>Salvar</Button>
+        </View>
 
 
-            ) :
-                (<View style={styles.viewError}>
-                    <Text style={styles.textError}>Network Error</Text>
-                </View>)
-            }
-        </>
-    )
+      ) :
+        (<View style={styles.viewError}>
+          <ActivityIndicator size='large' color='#666' />
+        </View>)
+      }
+    </>
+  )
 }
 
 export default Dashboard;
