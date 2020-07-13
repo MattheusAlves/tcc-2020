3019c549035d5c8efed0b6a54df47903a7fd6e19
Matheus Alves Domingos <matheusad@hmmg.local>
2020-07-13 13:56:18 -0300

    Init topic view

---
+++ frontend/src/pages/Topics/Topic/style.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Dimensions } from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    chipContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: "flex-start",
+        margin: 5
+    },
+    chip: {
+        width: 'auto',
+        justifyContent: "center"
+
+    },
+  
+    searchbar: {
+        marginTop: 20,
+        margin: 3,
+    },
+    topics: {
+
+    }
+
+
+})
+
+export default styles
