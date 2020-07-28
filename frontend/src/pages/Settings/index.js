831e090aca17460f2c0e9fac3fa1f4ec5b02b277
Vinicius Fontes <vinny_fa2000@hotmail.com>
2020-07-28 00:07:08 -0300

    Add icons on List-itens and icon to back page.

--- frontend/src/pages/Settings/index.js
+++ frontend/src/pages/Settings/index.js
@@ -8,14 +8,19 @@ import { Avatar } from 'react-native-paper';
 import { DataTable } from 'react-native-paper';
 import { List } from 'react-native-paper';
 import { Button } from 'react-native-paper';
+import Modal from './Modal';
 
 
 const styleText = {
   color: 'black',
 }
 
-const Settings = () => {
+function teste() {
 
+  return <Modal></Modal>;
+}
+
+const Settings = () => {
 
   return (
     <View>
@@ -23,29 +28,36 @@ const Settings = () => {
       <Avatar.Image size={64} source={require('../../assets/images/photoProfile.png')} />
 
       <List.Accordion
-        title="Picture Profile"
-        left={props => <List.Icon {...props} icon="camera" />}>
-        <List.Item title="Visualizar Foto" />
-        <List.Item title="Trocar Foto" />
+        title="Perfil"
+        left={props => <List.Icon {...props} icon={require('../../assets/images/account-box.png')} />}>
+        <List.Item title="Editar" onClick={teste()} />
       </List.Accordion>
-
+      <Divider />
       <List.Accordion
-        title="Organização"
-        left={props => <List.Icon {...props} icon="folder" />}>
+        title="Cursos"
+        left={props => <List.Icon {...props} icon={require('../../assets/images/book-open-page-variant.png')} />}>
         <List.Item title="First item" />
-        <List.Item title="Second item" />
       </List.Accordion>
-
+      <Divider />
       <List.Accordion
-        title="Agenda"
-        left={props => <List.Icon {...props} icon="calendar" />}>
-        <List.Item title="First item" />
-        <List.Item title="Second item" />
+        title="Pagamentos"
+        left={props => <List.Icon {...props} icon={require('../../assets/images/cash-usd-outline.png')} />}>
+        <List.Item title="Editar" />
       </List.Accordion>
-
+      <Divider />
+      <List.Accordion
+        title="Conta"
+        left={props => <List.Icon {...props} icon={require('../../assets/images/key-variant.png')} />}>
+        <List.Item title="First item" />
+        <List.Item title="Excluir" />
+      </List.Accordion>
+      <Divider />
+      <Button icon={require('../../assets/images/subdirectory-arrow-left.png')}>
+        Voltar
+      </Button>
     </View>
   );
-
+  
 };
 
 export default Settings;
