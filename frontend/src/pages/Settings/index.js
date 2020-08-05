cb15cb7650e40d60cf2778540c083e35eed8b8de
Vinicius Fontes <vinny_fa2000@hotmail.com>
2020-08-05 15:38:55 -0300

    Finished layout page settings.

--- frontend/src/pages/Settings/index.js
+++ frontend/src/pages/Settings/index.js
@@ -14,54 +14,62 @@ import { Dimensions, View } from 'react-native'
 const DeviceWidth = Dimensions.get('window').width
 //import { Container } from 'native-base';
 
+
+
 const Settings = () => {
 
+  
   return (
-    <View style={{width:'60%', alignSelf: 'center'}} >
+    <View style={{width:'60%', height:'85%' , marginTop: '2px', alignSelf: 'center'}} >
       <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center',  flexWrap: 'wrap' }}>
 
         <View style={{
           width: '25%', height: '100%', borderRightWidth: 1, borderTopWidth: 1
           , borderLeftWidth: 1, borderBottomWidth: 1, backgroundColor: 'white'
         }} >
+
+           {/* Menu do lado esquerdo */}
           <Avatar.Image size={64} source={require('../../assets/images/photoProfile.png')} />
+        
           <List.Accordion
             title="Perfil"
             left={props => <List.Icon {...props} icon={require('../../assets/images/account-box.png')} />}>
-            <List.Item title="Editar" />
+            <List.Item title="Editar" style={{cursor:'pointer'}} />
           </List.Accordion>
           <Divider />
 
           <List.Accordion
             title="Cursos"
             left={props => <List.Icon {...props} icon={require('../../assets/images/book-open-page-variant.png')} />}>
-            <List.Item title="First item" />
+            <List.Item title="First item" style={{cursor:'pointer'}} />
           </List.Accordion>
           <Divider />
 
           <List.Accordion
             title="Pagamentos"
             left={props => <List.Icon {...props} icon={require('../../assets/images/cash-usd-outline.png')} />}>
-            <List.Item title="Editar" />
+            <List.Item title="Editar" style={{cursor:'pointer'}}/>
           </List.Accordion>
           <Divider />
 
           <List.Accordion
             title="Conta"
             left={props => <List.Icon {...props} icon={require('../../assets/images/key-variant.png')} />}>
-            <List.Item title="First item" />
-            <List.Item title="Excluir" />
+            <List.Item title="First item" style={{cursor:'pointer'}} />
+            <List.Item title="Excluir" style={{cursor:'pointer'}} />
           </List.Accordion>
           <Divider />
         </View>
 
+        {/* Título na div superior */}
         <View style={{ width: '75%',height:'100%', alignSelf:'stretch'}}>
-          <View style={{ height: 50, backgroundColor: 'white', borderRightWidth: 1, borderTopWidth: 1
+          <View style={{ height: '10%', backgroundColor: 'white', borderRightWidth: 1, borderTopWidth: 1
           , borderLeftWidth: 1, borderBottomWidth: 1 }} >
-            <Title style={{ textAlign:'center'}}>Título</Title>
+            <Title id="teste" style={{ textAlign:'center'}}>Título</Title>
             </View>
 
-          <View style={{ height: '88%', backgroundColor: 'white', borderRightWidth: 1, borderTopWidth: 1
+          {/* Título na div inferior - conteúdo */}
+          <View style={{ height: '91%', backgroundColor: 'white', borderRightWidth: 1, borderTopWidth: 1
           , borderLeftWidth: 1, borderBottomWidth: 1 }} >
             <Paragraph>Aqui será o conteúdo do menu escolhido à esquerda.</Paragraph>
           </View>
