e48c793e1a4930c366eff61c645c45febac0c33f
Vinicius Fontes <vinny_fa2000@hotmail.com>
2020-08-15 18:04:33 -0300

    Creating fields and buttons to Account page.

--- frontend/src/pages/Settings/Main/Components/Content/ContentConta.js
+++ frontend/src/pages/Settings/Main/Components/Content/ContentConta.js
@@ -9,10 +9,51 @@ export class ContentConta extends React.Component {
     render() {
         return (
 
-            <Text>
-                &nbsp;
-                Conteúdo da cpnta
-            </Text>
+            <View style={{ padding: '10% 142px', marginTop: '5%', margin: 'auto' }} >
+
+                <TextInput
+                    placeholder='Senha antiga'
+                    style={{
+                        height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
+                        borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
+                    }} />
+
+                <TextInput
+                    placeholder="Confirme a senha antiga"
+                    style={{
+                        height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
+                        borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
+                    }} />
+
+                <TextInput
+                    placeholder="Senha nova"
+                    style={{
+                        height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
+                        borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
+                    }} />      
+
+                     <TextInput
+                    placeholder="Confirme a senha nova"
+                    style={{
+                        height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
+                        borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
+                    }} />             
+
+                <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: '2%' }}>
+
+                    <Button style={{ marginLeft: '6%' }}
+                        mode="contained" onPress={() => console.log('Pressed')}>
+                        Salvar
+                    </Button>
+
+                    <Button style={{ marginLeft: '6%' }}
+                        mode="contained" onPress={() => console.log('Pressed')}>
+                        Limpar
+                    </Button>
+
+                </View>
+
+            </View>
         );
     };
 }
