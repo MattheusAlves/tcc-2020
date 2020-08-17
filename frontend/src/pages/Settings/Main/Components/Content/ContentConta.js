eb4c5bd81778554dde14352cbb50f6928f04586b
Vinicius Fontes <vinicius.fontesdeandrade@gmail.com>
2020-08-17 12:05:09 -0300

    Add option delete account in Account menu.

--- frontend/src/pages/Settings/Main/Components/Content/ContentConta.js
+++ frontend/src/pages/Settings/Main/Components/Content/ContentConta.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Text, TextInput, Button } from 'react-native-paper';
+import { Text, List, TextInput, Button } from 'react-native-paper';
 import { View } from 'react-native';
-
+import { FormControl, FormHelperText, FormControlLabel, Input, InputLabel } from '@material-ui/core';
 
 export class ContentConta extends React.Component {
 
@@ -11,47 +11,73 @@ export class ContentConta extends React.Component {
 
             <View style={{ padding: '10% 142px', marginTop: '5%', margin: 'auto' }} >
 
-                <TextInput
-                    placeholder='Senha antiga'
-                    style={{
-                        height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
-                        borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
-                    }} />
+                <List.Accordion
+                    title="Configuração Senha"
+                    style={{ width: '100%' }}
+                    left={props => <List.Icon {...props} /*icon={require('../../../../../assets/images/lock.png')} */ />}>
 
-                <TextInput
-                    placeholder="Confirme a senha antiga"
-                    style={{
-                        height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
-                        borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
-                    }} />
+                    <TextInput
+                        placeholder='Senha antiga'
+                        style={{
+                            height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
+                            borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
+                        }} />
 
-                <TextInput
-                    placeholder="Senha nova"
-                    style={{
-                        height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
-                        borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
-                    }} />      
 
-                     <TextInput
-                    placeholder="Confirme a senha nova"
-                    style={{
-                        height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
-                        borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
-                    }} />             
+                    <TextInput
+                        placeholder="Confirme a senha antiga"
+                        style={{
+                            height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
+                            borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
+                        }} />
 
-                <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: '2%' }}>
+                    <TextInput
+                        placeholder="Senha nova"
+                        style={{
+                            height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
+                            borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
+                        }} />
 
-                    <Button style={{ marginLeft: '6%' }}
-                        mode="contained" onPress={() => console.log('Pressed')}>
-                        Salvar
+                    <TextInput
+                        placeholder="Confirme a senha nova"
+                        style={{
+                            height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
+                            borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
+                        }} />
+
+                    <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: '2%' }}>
+
+                        <Button style={{ marginLeft: '6%' }}
+                            mode="contained" onPress={() => console.log('Pressed')}>
+                            Salvar
                     </Button>
 
-                    <Button style={{ marginLeft: '6%' }}
-                        mode="contained" onPress={() => console.log('Pressed')}>
-                        Limpar
+                        <Button style={{ marginLeft: '6%' }}
+                            mode="contained" onPress={() => console.log('Pressed')}>
+                            Limpar
                     </Button>
 
-                </View>
+                    </View>
+                </List.Accordion>
+
+                <List.Accordion
+                    title="Deletar Conta"
+                    style={{ width: '100%' }}
+                    left={props => <List.Icon {...props} /*icon={require('../../../../../assets/images/lock.png')} */ />}>
+
+                    <TextInput
+                        placeholder="Senha atual"
+                        style={{
+                            height: '30px', width: '100%', borderColor: 'gray', backgroundColor: 'white',
+                            borderWidth: 0.5, marginTop: '1%', marginBottom: '1%'
+                        }} />
+
+                    <Button style={{ textAlign:'center'}}
+                        mode="contained" onPress={() => console.log('Pressed')}>
+                        Deletar conta
+                    </Button>
+
+                </List.Accordion>
 
             </View>
         );
