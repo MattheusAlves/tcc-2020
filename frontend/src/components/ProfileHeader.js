6f461dd9fe30ac5d3db597c34d32a753d830ecc2
Matheus2012 <matheus_alves0101@outlook.com>
2020-09-19 10:57:36 -0300

    finished  header

--- frontend/src/components/ProfileHeader.js
+++ frontend/src/components/ProfileHeader.js
@@ -1,36 +1,50 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet, Animated } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, Animated, TouchableOpacity,SafeAreaView } from 'react-native';
 import Svg, { Path } from 'react-native-svg'
 import { Avatar } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/AntDesign'
 
-const components = (props) => {
+const components = (props,{navigation}) => {
     return (
-        <Animated.View style={styles.container} style={{
-            transform: [{
-                scaleY: props.scrollY.interpolate({
-                    inputRange: [10, 140],
-                    outputRange: [1, .6],
-                    extrapolate: 'clamp'
-                }),
-                translateY: props.scrollY.interpolate({
-                    inputRange: [10, 140],
-                    outputRange: [0, -105 / 3],
-                    extrapolate: 'clamp'
-                })
-            }],
-        
-        
-                } }>
-            <View style={styles.header}>
-                <Svg width="410" height="101" fill="none" xmlns="http://www.w3.org/2000/svg" style={styles.svg}>
-                    <Path fill-rule="evenodd" clip-rule="evenodd" d="M0 45.0163L17 46.8537C34 48.6911 69 52.3659 103 45.0163C137 37.6667 171 19.2927 206 24.8049C240 30.3171 274 60.6341 308 60.6341C343 60.6341 377 30.3171 394 14.6992L411 0V113H394C377 113 343 113 308 113C274 113 240 113 206 113C171 113 137 113 103 113C69 113 34 113 17 113H0V45.0163Z" fill="#0099FF" stroke="#F7EFEF" stroke-width="2" />
-                </Svg>
-                <View style={styles.content}>
-                    <Avatar.Text size={74} label='MA' style={styles.avatar} />
-                    <Text style={styles.username}>Matheus Alves</Text>
-                </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.topNavigator} >
+                <TouchableOpacity style={styles.backButton}>
+                    <Icon size={45} name="leftcircle" color='white' />
+                </TouchableOpacity>
             </View>
-        </Animated.View >
+            <Animated.View style={[styles.container, {
+                transform: [{
+                    translateY: props.scrollY.interpolate({
+                        inputRange: [10, 140],
+                        outputRange: [0, -75 / 3],
+                        extrapolate: 'clamp'
+                    })
+                }]
+            }
+            ]}>
+                <Animated.View style={styles.header}>
+                    <Svg  height="101" fill="none"  xmlns="http://www.w3.org/2000/svg" style={styles.svg}>
+                        <Path fill-rule="evenodd" clip-rule="evenodd" d="M0 45.0163L17 46.8537C34 48.6911 69 52.3659 103 45.0163C137 37.6667 171 19.2927 206 24.8049C240 30.3171 274 60.6341 308 60.6341C343 60.6341 377 30.3171 394 14.6992L411 0V113H394C377 113 343 113 308 113C274 113 240 113 206 113C171 113 137 113 103 113C69 113 34 113 17 113H0V45.0163Z" fill="#0099FF" stroke="white" strokeWidth="2.2" />
+                    </Svg>
+                    <View style={styles.userInformation}>
+                        <Text style={styles.username} numberOfLines={1} ellipsizeMode="tail">Matheus Alves Domingos</Text>
+                        <View/>
+                        <Avatar.Text size={74} label='MA' style={styles.avatar} />
+                    </View>
+                </Animated.View>
+            </Animated.View>
+            <Animated.View style={[styles.content, {
+                transform: [{
+                    translateY: props.scrollY.interpolate({
+                        inputRange: [10, 140],
+                        outputRange: [80, 55],
+                        extrapolate: 'clamp'
+                    })
+                }]
+            }]}>
+                {props.children}
+            </Animated.View>
+        </SafeAreaView>
     )
 }
 
@@ -38,49 +52,56 @@ export default components;
 
 const styles = StyleSheet.create({
     container: {
-        // flex: 1,
-        // flexDirection: 'row',
         flexWrap: 'wrap',
-        width: Dimensions.get('screen').width,
-        borderBottomWidth: 3,
-        borderBottomColor: 'lightgray',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 4,
-        },
-        shadowOpacity: 0.30,
-        shadowRadius: 4.65,
-        elevation: 8,
-        zIndex:10
+        borderColor: "transparent",
+        position: 'absolute',
+        zIndex: 10,
     },
-    content: {
-        flex: 1,
+    topNavigator: {
+        zIndex:30
+    },
+    userInformation: {
         position: 'absolute',
         flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        left: 48,
-
+        top: 40,
+        right:5,
+        left:0
+    },
+    content: {
+        flex: 1
     },
     svg: {
-        width: Dimensions.get('screen').width
+        width: Dimensions.get('screen').width,
+
     },
     avatar: {
+        position:'absolute',
         borderWidth: 2.2,
         borderColor: 'white',
         backgroundColor: '#0099FF',
+        right:0,
     },
     header: {
         justifyContent: 'center',
-       
-        backgroundColor: 'rgba(0,153,255,.4)'
+        position: 'absolute',
+        backgroundColor: 'rgba(0,153,255,.4)',
+        paddingTop: 30
+    },
+    backButton:{
+        width:50,
+        marginLeft:3,
+        marginTop:3,
+        alignItems:'center',
+        justifyContent:'center',
+        padding:2
     },
     username: {
         fontSize: 24,
         color: 'white',
-        marginLeft: 9,
+        left:5,
+        top:50,
         alignSelf: 'flex-end',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        width:300
     }
 })
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -7,28 +7,33 @@ import Header from '../../components/ProfileHeader'
 const Profile = () => {
     const [scrollY] = useState(new Animated.Value(0))
     return (
-        <View style={{ flex: 1,zIndex:0 }}>
-            <View>
-            <Header scrollY={scrollY} style={{zIndex:10 }} />
-            </View>
-            <Animated.ScrollView
-                scrollEventThrottle={16}
-                onScroll={Animated.event([{
-                    nativeEvent: {
-                        contentOffset: { y: scrollY }
-                    },
 
-                }],
-                    { listener: (event) => { console.log(event.nativeEvent.contentOffset.y)
-                     }, useNativeDriver: true })}
-                contentContainerStyle={{ paddingBottom: 50,zIndex:0 }}>
-                <View style={styles.box}></View>
-                <View style={styles.box}></View>
-                <View style={styles.box}></View>
-                <View style={styles.box}></View>
-                <View style={styles.box}></View>
-            </Animated.ScrollView>
-        </View>
+        <Header scrollY={scrollY}>
+            <View style={{ flex: 1}}>
+
+                <Animated.ScrollView
+                    scrollEventThrottle={16}
+                    onScroll={Animated.event([{
+                        nativeEvent: {
+                            contentOffset: { y: scrollY }
+                        },
+
+                    }],
+                        {
+                            listener: (event) => {
+                                console.log(event.nativeEvent.contentOffset.y)
+                            }, useNativeDriver: true
+                        })}
+                    contentContainerStyle={{ paddingBottom: 50, zIndex: 0 }}
+                    style={{ zIndex: 0 }}>
+                    <View style={styles.box}></View>
+                    <View style={styles.box}></View>
+                    <View style={styles.box}></View>
+                    <View style={styles.box}></View>
+                    <View style={styles.box}></View>
+                </Animated.ScrollView>
+            </View>
+        </Header>
     )
 }
 
